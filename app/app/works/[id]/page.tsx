4d140ab6e08c3151bfb76b6627286aad6f50ee0c
TeruiYuuki <teriyaki0839@gmail.com>
2025-10-10 04:50:42 +0900

    feat: 詳細ページ作成

---
+++ app/app/works/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getDetailWork = async (id: number) => {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: work } = await supabase
+    .from("works")
+    .select("*")
+    .eq("id", id)
+    .single();
+  return work;
+};
+
+const WorkPage = async ({ params }: { params: { id: number } }) => {
+  const work = await getDetailWork(params.id);
+  return <h1>{work?.title}</h1>;
+};
+
+export default WorkPage;
