c76e7a616828206e3454256da4c845abcedbb49f
TeruiYuuki <teriyaki0839@gmail.com>
2025-11-03 15:31:54 +0900

    refactor: fetchするURLを相対URLに変更

--- app/components/checkout/SubscriptionButtion.tsx
+++ app/components/checkout/SubscriptionButtion.tsx
@@ -10,7 +10,7 @@ export const SubscriptionButtion = ({
   planId: string;
 }) => {
   const handleSubscribed = async () => {
-    await fetch(`http://localhost:3000/api/subscription/${planId}`);
+    const response = await fetch(`/api/subscription/${planId}`);
   };
 
   return (
